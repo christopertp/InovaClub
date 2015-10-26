e4ab0fa9147373b7f84468289458b190f7dd6b6f
christoper <christoper.t.p@gmail.com>
2015-10-26 12:51:28 +0700

    commit kedua

--- src/inovaclub/InovaClub.java
+++ src/inovaclub/InovaClub.java
@@ -16,6 +16,8 @@ public class InovaClub {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+	System.out.println("Perubahan Pertama");
     }
     
 }
